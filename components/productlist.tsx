133074584f1efc4a41528bdeaca407f5cd4f603d
vaibhav <vibhuuu05@gmail.com>
2025-05-04 17:37:06 +0530

    product info

---
+++ components/productlist.tsx
@@ -0,0 +1,20 @@
+import Stripe from "stripe";
+
+interface Props {
+  products: Stripe.Product[];
+}
+
+export const ProductList = ({ products }: Props) => {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Search products..." />
+      </div>
+      <ul>
+        {products.map((product) => {
+          return <li key={product.id}></li>;
+        })}
+      </ul>
+    </div>
+  );
+};
