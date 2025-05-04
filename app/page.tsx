8ef3d2989ccbd0427ea14f7ea0a97f50623c41b3
vaibhav <vibhuuu05@gmail.com>
2025-05-04 12:42:33 +0530

    important file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -8,6 +11,25 @@ export default async function Home() {
     limit: 3,
   });
 
-  console.log(products);
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>
+              Welcome to our ecommerce store
+            </h2>
+            <p>
+              Shop our wide selection of products
+            </p>
+            <Button asChild variant="default">
+              <Link href="/product">Our Products</Link>
+            </Button>
+           
+          </div>
+          <Image alt="Banner" src={products.data[0].images[0]} width={500} height={500}/>
+        </div>
+      </section>
+    </div>
+  );
 }
--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,2 @@
 import Stripe from "stripe";
-console.log("STRIPE_SECRET_KEY:", process.env.STRIPE_SECRET_KEY);
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);       
